4084542f840f0595468112e02b2342ad7193cab8
Joaquin Stur <joaquinstur@gmail.com>
2025-07-30 11:46:53 -0300

    Termina navbar basico
    Sin funcionalidades por ahora, pero para ir armando algo.

--- project/landing-page/src/App.jsx
+++ project/landing-page/src/App.jsx
@@ -1,9 +1,14 @@
 // crear componentes navbar, hero y footer
 
-import { Navbar } from "./components/Navbar";
+import { Navbar } from "./components/Navbar.jsx";
 
 export function App (){
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <main className="p-8">
+        <h2> el texto de abajo de la navbar </h2>
+      </main>
+    </>
   )
 }
--- project/landing-page/src/components/Navbar.jsx
+++ project/landing-page/src/components/Navbar.jsx
@@ -1,3 +1,21 @@
-export function Navbar (){
+import { HomeModernIcon } from '@heroicons/react/24/solid'
 
+export function Navbar (){
+  return(
+  <header className="bg-white shadow-md">
+    <div className='max-w-6xl mx-auto px-4 py-4 flex items-center justify-between '>
+      <div className='flex items-center space-x-2'>
+        <HomeModernIcon className='h-8 w-8 text-sky-600'/>
+        <h1 className='text-xl font-bold'>Inmobiliaria Hello World</h1>       
+      </div>
+      <nav className='space-x-6 hidden md:flex text-gray-700 font-medium'>
+        <a href="#" className='hover:text-blue-600'>Propiedades</a>
+        <a href="#" className='hover:text-blue-600'>Contacto</a>
+        <a href="#" className='hover:text-blue-600'>Nosotros</a>
+      </nav>
+    </div>
+  </header>
+  )
 }
+
+//href de los <a> 
